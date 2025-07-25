4e8eee3a60e8be03fd0fe69461d12c1a7e762344
Карпенко Александр Павлович <karpenko-a-p@mail.ru>
2025-07-25 15:40:17 +0300

    Добавил MemoryCache

---
+++ src/application/services/MemoryCache.ts
@@ -0,0 +1,85 @@
+import { Service } from 'typedi';
+import { Bind } from 'application/decorators';
+import 'server-only';
+
+class CachedValue {
+  constructor(
+    public readonly value: unknown,
+    private readonly expiresAt: number,
+    private readonly tags: string[],
+  ) {}
+
+  get expired(): boolean {
+    return this.expiresAt < Date.now();
+  }
+
+  hasTag(tag: string): boolean {
+    return this.tags.includes(tag);
+  }
+}
+
+@Service()
+export class MemoryCache {
+  private readonly cache = new Map<string, CachedValue>();
+
+  constructor() {
+    setInterval(this.invalidate, 60_000);
+  }
+
+  cacheForMinutes(minutes: number): number {
+    return Date.now() + 1000 * 60 * minutes;
+  }
+
+  cacheForHours(hours: number): number {
+    return Date.now() + 1000 * 60 * 60 * hours;
+  }
+
+  cacheForDays(days: number): number {
+    return Date.now() + 1000 * 60 * 60 * 24 * days;
+  }
+
+  @Bind()
+  has(key: string): boolean {
+    return this.cache.has(key);
+  }
+
+  @Bind()
+  get<TValue>(key: string): Nullable<TValue> {
+    const cachedValue = this.cache.get(key);
+
+    if (!cachedValue) return null;
+
+    if (cachedValue.expired) {
+      this.deleteByKey(key);
+      return null;
+    }
+
+    return cachedValue.value as TValue;
+  }
+
+  @Bind()
+  set(key: string, value: unknown, expiresAt?: number, tags?: string[]): void {
+    const cachedValue = new CachedValue(value, expiresAt ?? this.cacheForMinutes(10), tags ?? []);
+
+    if (!cachedValue.expired) this.cache.set(key, cachedValue);
+  }
+
+  @Bind()
+  deleteByKey(key: string): void {
+    this.cache.delete(key);
+  }
+
+  @Bind()
+  deleteByTag(tag: string): void {
+    this.cache.entries().forEach(([key, cachedValue]) => {
+      if (cachedValue.hasTag(tag)) this.deleteByKey(key);
+    });
+  }
+
+  @Bind()
+  invalidate(): void {
+    this.cache.entries().forEach(([key, cachedValue]) => {
+      if (cachedValue.expired) this.deleteByKey(key);
+    });
+  }
+}
